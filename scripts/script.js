31801aaa688b15109ea33b8247d30431d9170182
Levi Tan Ong <levi@byimplication.com>
2014-03-19 01:02:43 +0800

    updates foundation, adds highcharts-ng

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-var app = angular.module('recon', []);
+var app = angular.module('recon', ['highcharts-ng']);
 
 // values and constants
 
@@ -333,8 +333,35 @@ app.controller('Main', function($scope, users, requests, sampleData){
   
 });
 
-app.controller('Overview', function($scope){
+app.controller('Overview', function($scope, requests){
+  $scope.chartSeries = [
+    {"name": "Some data", "data": [1, 2, 4, 7, 3]},
+    {"name": "Some data 3", "data": [3, 1, null, 5, 2], connectNulls: true},
+    {"name": "Some data 2", "data": [5, 2, 2, 3, 5], type: "column"},
+    {"name": "My Super Column", "data": [1, 1, 2, 3, 2], type: "column"}
+  ];
 
+
+  $scope.chartConfig = {
+    options: {
+      chart: {
+        type: 'areaspline'
+      },
+      plotOptions: {
+        series: {
+          stacking: ''
+        }
+      }
+    },
+    series: $scope.chartSeries,
+    title: {
+      text: 'Hello'
+    },
+    credits: {
+      enabled: true
+    },
+    loading: false
+  }
 });
 
 app.controller('List', function($scope, users, requests, levels, filters, types){
