f04602cff53c2a38c0c71f2387d870a421336c0c
Levi Tan Ong <levi@byimplication.com>
2014-03-24 23:41:55 +0800

    refine model init

--- scripts/script.js
+++ scripts/script.js
@@ -205,12 +205,15 @@ app.factory('sampleData', function(types, users, requests, responses, $http){
     genUsers: function(qty){
       var backupUsers = {"results":[{"user":{"gender":"female","name":{"title":"miss","first":"dora","last":"little"},"location":{"street":"8070 lakeview st","city":"roseburg","state":"kansas","zip":"51648"},"email":"dora.little60@example.com","username":"ticklishelephant593","password":"121212","salt":"TL3[@F_T","md5":"abbb59295aab4d1dfe460c0d8db9d9c3","sha1":"7aa93a9581897e59906fc666300d76630fe3968f","sha256":"fb4637934a85bbd765e0d08a0065adf63608620b424e349005d19a8c98f8b4bd","registered":"1059126112","dob":"25717879","phone":"(572)-318-2418","cell":"(280)-601-3392","SSN":"350-39-1332","picture":"backupUsers/women/30.jpg"},"seed":"ce2b8529023508c","version":"0.3.2"},{"user":{"gender":"female","name":{"title":"mrs","first":"judy","last":"west"},"location":{"street":"8760 photinia ave","city":"fountain valley","state":"massachusetts","zip":"92342"},"email":"judy.west50@example.com","username":"blackkoala874","password":"domino","salt":"ZF$QrMFZ","md5":"5be03d5fe05cb671fc0bfae4cbb53164","sha1":"d0b7a789d1908c93acb4d165f6221a5308116459","sha256":"204d082383565a0bfaefea25ddc4325cf76ff0741e13beab9450d95b0a541fd3","registered":"1384311792","dob":"331011243","phone":"(908)-287-7543","cell":"(258)-148-4457","SSN":"255-19-6093","picture":"backupUsers/women/29.jpg"},"seed":"8b9f2fb636338d4","version":"0.3.2"},{"user":{"gender":"male","name":{"title":"mr","first":"bryan","last":"george"},"location":{"street":"4017 blue island ave","city":"magna","state":"new york","zip":"69672"},"email":"bryan.george43@example.com","username":"bluepanda291","password":"asdfasdf","salt":"","md5":"6a204bd89f3c8348afd5c77c717a097a","sha1":"92429d82a41e930486c6de5ebda9602d55c39986","sha256":"2413fb3709b05939f04cf2e92f7d0897fc2596f9ad0b8a9ea855c7bfebaae892","registered":"966276524","dob":"22723478","phone":"(365)-510-4602","cell":"(437)-643-4832","SSN":"747-24-6019","picture":"backupUsers/men/24.jpg"},"seed":"72ef73303fa008d","version":"0.3.2"},{"user":{"gender":"male","name":{"title":"mr","first":"floyd","last":"welch"},"location":{"street":"6260 eagle point rd","city":"rockford","state":"new hampshire","zip":"59940"},"email":"floyd.welch39@example.com","username":"purplekoala243","password":"lennon","salt":"GNHXkh:H","md5":"6fd10f8eedb71d2446e324fcc69bfd15","sha1":"3491477bbac65fb0f6bf5b30f34ecfea80809dd6","sha256":"119562e47bf056cb8b8029d312d91ca4c57aaf111c32b8e8d1fbde5d85616bf5","registered":"1266144183","dob":"109056973","phone":"(896)-222-8460","cell":"(484)-608-1738","SSN":"905-21-5507","picture":"backupUsers/men/19.jpg"},"seed":"2949c52e25e6bfc","version":"0.3.2"},{"user":{"gender":"male","name":{"title":"mr","first":"alex","last":"reed"},"location":{"street":"7121 texas ave","city":"sunnyvale","state":"new jersey","zip":"91802"},"email":"alex.reed17@example.com","username":"tinybutterfly964","password":"wesley","salt":":1ZNO?Le","md5":"97615a72f8f4e7167deface7318123fa","sha1":"23faa3b06c61f5a53da5fb0f33755843a7544f91","sha256":"1788e40aa3da2eaafe2193533bf514c2f7980626a3760f8e33066617d918c7e6","registered":"937492198","dob":"380566097","phone":"(623)-132-3167","cell":"(183)-619-6929","SSN":"421-34-9252","picture":"backupUsers/men/13.jpg"},"seed":"66dd0304b80175a","version":"0.3.2"}]};
       var self = this;
-      var populate = function(results, qty, generator){
+      var populate = function(results, qty){
         if(!qty) qty = 1;
         for(var i = 0; i < qty; i++){
-          var u = self.genUser(i);
-          u.picture = results[i].user.picture;
-          u.name = results[i].user.name;
+          // var u = self.genUser(i);
+
+          var u = new users.User(results[i].user, i);
+
+          // u.picture = results[i].user.picture;
+          // u.name = results[i].user.name;
           users.list.push(u);
         }
         users.current = users.list[0];
@@ -221,10 +224,10 @@ app.factory('sampleData', function(types, users, requests, responses, $http){
       }
       $http({method: 'GET', url: 'http://api.randomuser.me/?results=' + qty})
         .success(function(data){
-          populate(data.results, qty, self);
+          populate(data.results, qty);
         })
         .error(function(){
-          populate(backupUsers.results, qty, self);
+          populate(backupUsers.results, qty);
         });
     },
     genResponses: function(){
@@ -255,8 +258,30 @@ app.factory('sampleData', function(types, users, requests, responses, $http){
 
 app.factory('users', function(){
   var u = {
-    list: [],
-    current: {},
+    User: function(data, level){
+      var self = this;
+      for(prop in data){
+        this[prop] = data[prop];
+      }
+      this.level = level;
+      this.address = {
+        district: null,
+        department: null,
+        region: 8,
+        province: 'Western Samar',
+        city: null,
+        town: 'Daram',
+        barangay: null,
+        sitio: null
+      };
+      this.getName = function(){
+        if(typeof(this.name) == "undefined"){
+          return "Guest";
+        } else {
+          return this.name.first + " " + this.name.last;
+        }
+      };
+    },
     isLoggedIn: true,
     switchTo: function(u){
       this.current = u;
@@ -267,6 +292,8 @@ app.factory('users', function(){
       this.isLoggedIn = false;
     }
   }
+  u.current = new u.User();
+  u.list = [];
   return u;
 });
 
